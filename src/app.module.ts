6d7f600516c980d47289246f464481574a2068ef
dimeBrii <thiswasbrii@gmail.com>
2024-10-20 11:10:05 -0400

    ✨Feat : addingf rabbit mq and basic docker config

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,16 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { RabbitMQModule } from './config/rabbitmq/rabbitmq.module';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    RabbitMQModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
