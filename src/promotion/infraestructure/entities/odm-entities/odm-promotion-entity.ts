939afe530fbdc39828734128c52d1cb2d252d442
C102002 <alfnicolasfff@hotmail.com>
2025-01-13 21:56:56 -0400

    feat: :construction: improving crqrs in products, bundles and promotions

--- src/promotion/infraestructure/entities/odm-entities/odm-promotion-entity.ts
+++ src/promotion/infraestructure/entities/odm-entities/odm-promotion-entity.ts
@@ -1,5 +1,5 @@
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
-import { Document } from 'mongoose';
+import mongoose, { Document } from 'mongoose';
 import { PromotionStateEnum } from 'src/promotion/domain/value-object/enum/promotion-state.enum';
 import { IOdmModelPromotion } from '../../model-entity/odm-model-entity/odm-promotion-interface';
 import { OdmProduct } from 'src/product/infraestructure/entities/odm-entities/odm-product-entity';
@@ -22,10 +22,10 @@ export class OdmPromotionEntity extends Document implements IOdmModelPromotion{
     @Prop({ type: Number, required: true })
     discount: number;
 
-    @Prop({ type: [{ type: String, ref: 'OdmProduct' }], default: [] })
+    @Prop({ type: [{ type: String, ref: 'odmproduct', localField: 'productId', foreignField: 'id' }], default: [] })
     products?: OdmProduct[];
 
-    @Prop({ type: [{ type: String, ref: 'OdmBundle' }], default: [] })
+    @Prop({ type: [{ type: String, ref: 'odmbundle', localField: 'bundleId', foreignField: 'id' }], default: [] })
     bundles?: OdmBundle[];
 
 }
